8bfe83b37b007c00b9d784650915371c0ef31714
alisher134 <alisherrakhmanov134@gmail.com>
2024-10-18 22:00:38 +0500

    feat: add swagger api description

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
@@ -7,10 +8,18 @@ async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule);
 	const config = app.get(ConfigService);
 
+	const options = new DocumentBuilder()
+		.setTitle('CurseHunter')
+		.setDescription('The CurseHunter API description')
+		.setVersion('1.0')
+		.build();
+	const documentFactory = (): OpenAPIObject => SwaggerModule.createDocument(app, options);
+	SwaggerModule.setup('api', app, documentFactory);
+
 	app.use(cookieParser());
 	app.setGlobalPrefix('api/v1');
 	app.enableCors({
-		allowedHeaders: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+		origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
 		credentials: true,
 		exposedHeaders: ['set-cookie']
 	});
